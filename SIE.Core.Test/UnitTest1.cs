c441188f9c12332a4040620800035d8524439bf8
Juan Manuel Pardo Garcia <juan.pardogarcia@plexus.tech>
2024-04-24 00:47:03 +0200

    Create the Tests project for the main Library of the Comprehensive System for Business

---
+++ SIE.Core.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SIE.Core.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
